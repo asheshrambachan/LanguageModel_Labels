5d70f9b6bfe88b0899ce4218e0426116d4f5de08
hayaalsh <haya1@mit.edu>
2024-09-20 12:10:10 -0400

    [CB v2] Updated 08_results code

--- congressional_bills_v2/Code/01_bills_cleaning.py
+++ congressional_bills_v2/Code/01_bills_cleaning.py
@@ -4,160 +4,107 @@ import numpy as np
 import matplotlib.pyplot as plt
 from copy import deepcopy
 
-# Define directories
-repo_dir = '/Users/haya1/Documents/LanguageModel_Labels/congressional_bills_v2'
-os.chdir(repo_dir)
+REPO_DIR = '/Users/haya1/Documents/LanguageModel_Labels/congressional_bills_v2'
 
-data_dir = os.path.join(repo_dir, 'Data')
-os.makedirs(data_dir, exist_ok=True)
+def load_cap(path_cap):
+    cap = pd.read_csv(path_cap, low_memory=False)
+    return(cap)
 
-fig_dir = os.path.join(repo_dir, 'Figures and Tables')
-os.makedirs(fig_dir, exist_ok=True)
+def clean_cap(cap):
+    # Correct BillID
+    cap['BillID'] = cap['cong'].astype(str) + '-' + cap['bill_type'].apply(lambda x: x.upper()) + '-' + cap['bill_no'].astype(str) 
 
-# path_cap = 'https://comparativeagendas.s3.amazonaws.com/datasetfiles/US-Legislative-congressional_bills_19.3_3_3.csv'
-# path_cbp_80_92 = 'http://congressionalbills.org/billfiles/bills80-92.zip'
-# path_cbp_93_114 = 'http://congressionalbills.org/billfiles/bills93-114.zip'
-path_cap = os.path.join(data_dir, 'CAP.csv')
-path_cbp_80_92 = os.path.join(data_dir, 'CBP80-92.txt')
-path_cbp_93_114 = os.path.join(data_dir, 'CBP93-114.csv')
+    # Rename columns for consistency
+    cap.rename(columns={
+        'majortopic': 'Major',
+        'description': 'Description',
+        'year': 'Year',
+        'party': 'Party', 
+        'pass_h': 'PassH', 
+        'pass_s': 'PassS',
+        'chamber': 'Chamber',
+        'cong': 'Cong',
+        'name_full': 'NameFull'
+        }, inplace=True)
 
-MAJOR_PAP2OURS = {
-     1:  1,
-     2:  2,
-     3:  3,
-     4:  4,
-     5:  5,
-     6:  6,
-     7:  7,
-     8:  8,
-     9:  9,
-    10: 10,
-    12: 11,
-    13: 12,
-    14: 13,
-    15: 14,
-    16: 15,
-    17: 16,
-    18: 17,
-    19: 18,
-    20: 19,
-    21: 20
-}
+    # Correct chamber encoding for the 114th congress
+    if ~((cap.loc[cap['Cong']==114, 'Chamber'].unique()) == [1,2]).all():
+        cap.loc[cap['Cong']==114, 'Chamber'] = cap.loc[cap['Cong']==114, 'Chamber'] + 1
+    cap['Chamber'] = cap['Chamber'].apply(lambda x: int(x == 2)) # House = 1, Senate = 2 in CAP
 
-MAJOR_TEXT = pd.read_csv(os.path.join(data_dir, 'Codebooks/major_topics.csv')).set_index('Major')['MajorText'].to_dict()
+    # Recode Party 400 as Democratic (100) 
+    cap.loc[(cap['Party']==400) & (cap['NameFull']=='Anibal Acevedo-Vila'), 'Party'] = 100
 
-CHAMBER_CODE = {
-    0: 'House', 
-    1: 'Senate'
-}
+    # Keep columns we need
+    cap = cap[['BillID', 'Major', 'Description', 'Party', 'Chamber', 'Year', 'PassH', 'PassS']]
 
-PARTY_CODE = {
-    100: 'Democrat', 
-    112: 'Conservative',
-    200: 'Republican',
-    328: 'Independent'
-}
+    # Drop bills with missing data
+    cap.dropna(inplace=True)
 
-# Clean CAP data ------------------------------------------------------
-# Load CAP data
-cap = pd.read_csv(path_cap, low_memory=False)
+    # Remove duplicates based on bill ID
+    cap.drop_duplicates(subset='BillID', inplace=True)
 
-# Correct BillID
-cap['BillID'] = cap['cong'].astype(str) + '-' + cap['bill_type'].apply(lambda x: x.upper()) + '-' + cap['bill_no'].astype(str) 
+    # Make Major, PassH and PassS integers
+    cap['Major']  = cap['Major'].apply(lambda x: int(x))
+    cap['PassH']  = cap['PassH'].apply(lambda x: int(x))
+    cap['PassS']  = cap['PassS'].apply(lambda x: int(x))
 
-# Rename columns for consistency
-cap.rename(columns={
-    'majortopic': 'Major',
-    'description': 'Description',
-    'year': 'Year',
-    'party': 'Party', 
-    'pass_h': 'PassH', 
-    'pass_s': 'PassS',
-    'chamber': 'Chamber',
-    'cong': 'Cong',
-    'name_full': 'NameFull'
-    }, inplace=True)
+    # # Correct Description
+    # cap['Description'] = cap['Description'].str.replace(r'[_]|[?"]{2,}|[`]|[\']', '', regex=True)
+    # cap['Description'] = cap['Description'].str.replace(r'\s+', ' ', regex=True)
+    return(cap)
 
-# Correct chamber encoding for the 114th congress
-if ~((cap.loc[cap['Cong']==114, 'Chamber'].unique()) == [1,2]).all():
-    cap.loc[cap['Cong']==114, 'Chamber'] = cap.loc[cap['Cong']==114, 'Chamber'] + 1
-cap['Chamber'] = cap['Chamber'].apply(lambda x: int(x == 2)) # House = 1, Senate = 2 in CAP
+def load_cbp(path_cbp_80_92, path_cbp_93_114):  
+    # Load CBP data, 80th through 92nd congress
+    cbp_80_92 = pd.read_csv(path_cbp_80_92, sep='\t', encoding='latin-1', low_memory=False)
+    cbp_80_92.rename(columns={' ': 'Minor'}, inplace=True)
+    cbp_80_92['DW1'] = cbp_80_92['DW1'].apply(lambda x: np.nan if x == -99.0 else x)
 
-# Recode Party 400 as Democratic (100) 
-cap.loc[(cap['Party']==400) & (cap['NameFull']=='Anibal Acevedo-Vila'), 'Party'] = 100
+    # Load CBP data, 93rd through 114th congress
+    cbp_93_114 = pd.read_csv(path_cbp_93_114, sep=';', encoding='latin-1', low_memory=False)
+    cbp_93_114['DW1'] = cbp_93_114['DW1'].apply(lambda s: float(s.replace(',', '.') if isinstance(s, str) else s))
 
-# Keep columns we need
-cap = cap[['BillID', 'Major', 'Description', 'Party', 'Chamber', 'Year', 'PassH', 'PassS']]
+    # Merge CBP datasets
+    cbp = pd.concat([cbp_80_92, cbp_93_114], join='inner').reset_index(drop=True)
+    return(cbp)
 
-# Drop bills with missing data
-cap.dropna(inplace=True)
+def clean_cbp(cbp):
+    # Rename columns
+    cbp.rename(columns={'Title': 'Description'}, inplace=True)
 
-# Remove duplicates based on bill ID
-cap.drop_duplicates(subset='BillID', inplace=True)
+    # Correct BillID
+    cbp['BillType'] = cbp['BillType'].apply(lambda x: x.upper())
+    cbp['BillID'] = cbp['Cong'].astype(str) + '-' + cbp['BillType'] + '-' + cbp['BillNum'].astype(str) 
 
-# Make Major, PassH and PassS integers
-cap['Major']  = cap['Major'].apply(lambda x: int(x))
-cap['PassH']  = cap['PassH'].apply(lambda x: int(x))
-cap['PassS']  = cap['PassS'].apply(lambda x: int(x))
-
-# # Correct Description
-# cap['Description'] = cap['Description'].str.replace(r'[_]|[?"]{2,}|[`]|[\']', '', regex=True)
-# cap['Description'] = cap['Description'].str.replace(r'\s+', ' ', regex=True)
-
-# Clean CBP data ------------------------------------------------------
-# Load CBP data, 80th through 92nd congress
-cbp_80_92 = pd.read_csv(path_cbp_80_92, sep='\t', encoding='latin-1', low_memory=False)
-cbp_80_92.rename(columns={' ': 'Minor'}, inplace=True)
-cbp_80_92['DW1'] = cbp_80_92['DW1'].apply(lambda x: np.nan if x == -99.0 else x)
-
-# Load CBP data, 93rd through 114th congress
-cbp_93_114 = pd.read_csv(path_cbp_93_114, sep=';', encoding='latin-1', low_memory=False)
-cbp_93_114['DW1'] = cbp_93_114['DW1'].apply(lambda s: float(s.replace(',', '.') if isinstance(s, str) else s))
-
-# Merge CBP datasets
-cbp = pd.concat([cbp_80_92, cbp_93_114], join='inner').reset_index(drop=True)
-
-# Rename columns
-cbp.rename(columns={'Title': 'Description'}, inplace=True)
-
-# Correct BillID
-cbp['BillType'] = cbp['BillType'].apply(lambda x: x.upper())
-cbp['BillID'] = cbp['Cong'].astype(str) + '-' + cbp['BillType'] + '-' + cbp['BillNum'].astype(str) 
-
-# Remove duplicates in CBP based on BillID, keeping the bill with Major code similar to that in CAP
-# 1. Identify duplicated BillIDs in CBP
-cbp.drop_duplicates(subset=['BillID', 'Major'], inplace=True)
-cbp_dup = cbp[cbp['BillID'].duplicated(keep=False)]
-
-for bill_id in cbp_dup['BillID'].unique():
-    # 2. Get the Major values from CAP for matching BillID
-    cap_major = cap.loc[cap['BillID']==bill_id, 'Major'].item()
-
-    # 3. Filter rows where the Major_CBP does not match the Major_CAP
-    condition = (cbp['BillID']==bill_id) & (cbp['Major']!=cap_major)
-
-    # 4. Remove the mismatched bills from cbp based on the condition
+    # Remove duplicates in CBP based on BillID, keeping the bill with Major code similar to that in CAP
+    cbp.drop_duplicates(subset=['BillID', 'Major'], inplace=True)
+    condition = (
+        ((cbp['BillID']=="107-HR-5715") & (cbp['Major']!=12)) |
+        ((cbp['BillID']=="107-S-3051") & (cbp['Major']!=9)) |
+        ((cbp['BillID']=="107-S-3160") & (cbp['Major']!=15))
+    ) # These are bills with Major_CBP != Major_CAP
     cbp = cbp[~condition]
-    
-# Impute missing DW1 scores
-cbp['DW1_NA'] = cbp['DW1']
-dw1 = cbp.groupby(['NameFull', 'DW1'], group_keys=True).first()['DW1_NA'] 
-dw1_mean = dw1.mean()
-print(dw1_mean)
-cbp.fillna(value={'DW1': dw1_mean}, inplace=True)
+        
+    # Impute missing DW1 scores
+    cbp['DW1_NA'] = cbp['DW1']
+    dw1 = cbp.groupby(['NameFull', 'DW1'], group_keys=True).first()['DW1_NA'] 
+    dw1_mean = dw1.mean()
+    # print(dw1_mean)
+    cbp.fillna(value={'DW1': dw1_mean}, inplace=True)
 
-# Keep columns we need
-cbp = cbp[['BillID', 'Major', 'Description', 'Party', 'Chamber', 'DW1', 'PassH', 'PassS', 'Postal']]
+    # Keep columns we need
+    cbp = cbp[['BillID', 'Major', 'Description', 'Party', 'Chamber', 'DW1', 'PassH', 'PassS', 'Postal']]
 
-# Drop bills with missing data
-cbp.dropna(inplace=True)
+    # Drop bills with missing data
+    cbp.dropna(inplace=True)
 
-# Make Majors an integer
-cbp['Major'] = cbp['Major'].apply(lambda x: int(x))
+    # Make Majors an integer
+    cbp['Major'] = cbp['Major'].apply(lambda x: int(x))
 
-# # Correct Description
-# cbp['Description'] = cbp['Description'].str.replace(r'[_]|[?"]{2,}|[`]|[\']', '', regex=True)
-# cbp['Description'] = cbp['Description'].str.replace(r'\s+', ' ', regex=True)
+    # # Correct Description
+    # cbp['Description'] = cbp['Description'].str.replace(r'[_]|[?"]{2,}|[`]|[\']', '', regex=True)
+    # cbp['Description'] = cbp['Description'].str.replace(r'\s+', ' ', regex=True)
+    return(cbp)
 
 # Merge CAP and CBP datasets ------------------------------------------------------
 def description_mismatch(d1, d2):
@@ -182,64 +129,160 @@ def description_mismatch(d1, d2):
     condition = description_cap!=description_cbp
     return condition
 
-bills = cap.merge(cbp, on=['BillID', 'Major', 'Party', 'PassS', 'PassH', 'Chamber'], suffixes=('', '_CBP'))
+def merge_cap_cbp(cap, cbp):
+    major_PAP2Ours = {
+        1:  1,
+        2:  2,
+        3:  3,
+        4:  4,
+        5:  5,
+        6:  6,
+        7:  7,
+        8:  8,
+        9:  9,
+        10: 10,
+        12: 11,
+        13: 12,
+        14: 13,
+        15: 14,
+        16: 15,
+        17: 16,
+        18: 17,
+        19: 18,
+        20: 19,
+        21: 20
+    }
+        
+    major_code = {
+         1: "Macroeconomics",
+         2: "Civil Rights, Minority Issues, and Civil Liberties",
+         3: "Health",
+         4: "Agriculture",
+         5: "Labor and Employment",
+         6: "Education",
+         7: "Environment",
+         8: "Energy",
+         9: "Immigration",
+        10: "Transportation",
+        11: "Law, Crime, and Family Issues",
+        12: "Social Welfare",
+        13: "Community Development and Housing Issues",
+        14: "Banking, Finance, and Domestic Commerce",
+        15: "Defense",
+        16: "Space, Science, Technology, and Communications",
+        17: "Foreign Trade",
+        18: "International Affairs and Foreign Aid",
+        19: "Government Operations",
+        20: "Public Lands and Water Management"
+    }
 
-# Drop bills with mismatching Description in CAP and CBP
-condition = description_mismatch(bills['Description'], bills['Description_CBP'])
-bills = bills[~condition] 
+    chamber_code = {
+        0: 'House', 
+        1: 'Senate'
+    }
 
-# Remove bills with Major=99 (~80,000)
-bills = bills[bills['Major']!=99]
+    party_code = {
+        100: 'Democrat', 
+        112: 'Conservative',
+        200: 'Republican',
+        328: 'Independent'
+    }
 
-# Remove duplicates based on Description (lower cased)
-bills['Description_lower'] = bills['Description'].apply(lambda x: x.lower())
-bills.drop_duplicates(subset='Description_lower', inplace=True)
+    bills = cap.merge(cbp, on=['BillID', 'Major', 'Party', 'PassS', 'PassH', 'Chamber'], suffixes=('', '_CBP'))
 
-# Map PAP/CAP major topic IDs to our IDs 
-bills['Major'] = bills['Major'].apply(lambda x: MAJOR_PAP2OURS[x])
+    # Drop bills with mismatching Description in CAP and CBP
+    condition = description_mismatch(bills['Description'], bills['Description_CBP'])
+    bills = bills[~condition] 
 
-# Add MajorText column 
-bills['MajorText'] = bills['Major'].apply(lambda x: MAJOR_TEXT[x])
+    # Remove bills with Major=99 (~80,000)
+    bills = bills[bills['Major']!=99]
 
-# Add Chamber codes
-bills['Chamber'] = bills['Chamber'].apply(lambda x: CHAMBER_CODE[x])
+    # Remove duplicates based on Description (lower cased)
+    bills['Description_lower'] = bills['Description'].apply(lambda x: x.lower())
+    bills.drop_duplicates(subset='Description_lower', inplace=True)
 
-# Add Party codes
-bills['Party'] = bills['Party'].apply(lambda x: PARTY_CODE[x])
+    # Map PAP/CAP major topic IDs to our IDs 
+    bills['Major'] = bills['Major'].apply(lambda x: major_PAP2Ours[x])
 
-# Rearrange columns
-bills = bills[['BillID', 'Year', 'Major', 'MajorText', 'Party', 'PassH', 'PassS', 'Description', 'DW1', 'Chamber', 'Postal']]
-bills.reset_index(drop=True)
-print(len(bills)) # n = 228387
+    # Add MajorText column 
+    bills['MajorText'] = bills['Major'].apply(lambda x: major_code[x])
 
-seed = 123
-n_examples = 5
-# Bills used for few-shot prompting
-examples = bills.groupby('Major').sample(n=1, random_state=seed).reset_index(drop=True)[:(n_examples*3)] 
-examples['ExampleSetNum'] = np.repeat([1, 2, 3], repeats=n_examples)
-examples.to_csv(os.path.join(data_dir, 'bills_examples.csv'), index=False)
-print(f'Saved bills_examples.csv, n = {len(examples)}, at {data_dir}')
+    # Add Chamber codes
+    bills['Chamber'] = bills['Chamber'].apply(lambda x: chamber_code[x])
 
-# examples01 = examples.iloc[:n_examples]
-# examples02 = examples.iloc[n_examples:(n_examples*2)]
-# examples03 = examples.iloc[(n_examples*2):]
-# examples01.to_csv(os.path.join(data_dir, 'bills_example1.csv'), index=False)
-# examples02.to_csv(os.path.join(data_dir, 'bills_example2.csv'), index=False)
-# examples03.to_csv(os.path.join(data_dir, 'bills_example3.csv'), index=False)
+    # Add Party codes
+    bills['Party'] = bills['Party'].apply(lambda x: party_code[x])
 
-condition = bills['BillID'].apply(lambda x: x not in examples['BillID'].to_list())
-bills = bills[condition]
-print(f'Removed {len(examples)} examples from bills data.')
+    # Rearrange columns
+    bills = bills[['BillID', 'Year', 'Major', 'MajorText', 'Party', 'PassH', 'PassS', 'Description', 'DW1', 'Chamber', 'Postal']]
+    bills.reset_index(drop=True)
+    print(len(bills)) # n = 228387
+    return()
 
-bills_10k = bills.sample(n=10_000, random_state=seed)
-bills_10k.to_csv(os.path.join(data_dir, 'bills_10k.csv'), index=False)
-print(f'Saved bills_10k.csv, n = {len(bills_10k)}, at {data_dir}')
+def sample_bills(bills, n_examples = 5, seed = 123):
+    # Bills used for few-shot prompting
+    examples = bills.groupby('Major').sample(n=1, random_state=seed).reset_index(drop=True)[:(n_examples*3)] 
 
-plt.figure(figsize=(10, 6))
-bills_10k['Year'].hist(bins=range(int(bills_10k['Year'].min()), int(bills_10k['Year'].max()) + 1, 2), edgecolor='black')
-plt.title('A histogram of the frequency of the 10K bills over years')
-plt.xlabel('Year')
-plt.ylabel('Number of Bills')
-plt.grid(False)
-plt.savefig(os.path.join(fig_dir, 'A histogram of the frequency of the 10K bills over years.png'))
-print(f'Saved figures at {fig_dir}')
+    condition = bills['BillID'].apply(lambda x: x not in examples['BillID'].to_list())
+    bills = bills[condition]
+    print(f'Removed {len(examples)} examples from bills data.')
+
+    bills_10k = bills.sample(n=10_000, random_state=seed)
+    return(bills_10k, examples)
+
+def main():
+    # Define directories
+    data_dir = os.path.join(REPO_DIR, 'Data')
+    fig_dir = os.path.join(REPO_DIR, 'Figures and Tables')
+    os.makedirs(data_dir, exist_ok=True)
+    os.makedirs(fig_dir, exist_ok=True)
+
+    # CAP and CBP data paths
+    path_cap = 'https://comparativeagendas.s3.amazonaws.com/datasetfiles/US-Legislative-congressional_bills_19.3_3_3.csv' 
+    path_cbp_80_92 = 'http://congressionalbills.org/billfiles/bills80-92.zip' 
+    path_cbp_93_114 = 'http://congressionalbills.org/billfiles/bills93-114.zip' 
+    # path_cap = os.path.join(data_dir, 'CAP.csv')
+    # path_cbp_80_92 = os.path.join(data_dir, 'CBP80-92.txt')
+    # path_cbp_93_114 = os.path.join(data_dir, 'CBP93-114.csv')
+    
+    # Load and clean CAP
+    cap = load_cap(path_cap)
+    cap = clean_cap(cap)
+    cap.to_csv(os.path.join(data_dir, "cap.csv"), index=False)
+    print(f'Saved cap.csv, n = {len(cap)}, at {data_dir}')
+
+    # Load and clean CBP
+    cbp = load_cbp(path_cbp_80_92, path_cbp_93_114)
+    cbp.to_csv(os.path.join(data_dir, "cbp2.csv"), index=False)
+    cbp = clean_cbp(cbp)
+    cbp.to_csv(os.path.join(data_dir, "cbp.csv"), index=False)
+    print(f'Saved cbp.csv, n = {len(cbp)}, at {data_dir}')
+
+    # Merge CAP and CBP data
+    bills = merge_cap_cbp(cap, cbp)
+    bills.to_csv(os.path.join(data_dir, "bills.csv"), index=False)
+    print(f'Saved bills.csv, n = {len(bills)}, at {data_dir}')
+
+    # Draw 10k bills and 5*3=15 bill examples used for few-shot prompts.
+    bills_10k, bills_examples = sample_bills(bills, n_examples = 15, seed = 123)
+    bills_10k.to_csv(os.path.join(data_dir, 'bills_10k.csv'), index=False)
+    print(f'Saved bills_10k.csv, n = {len(bills_10k)}, at {data_dir}')
+
+    # Split the 15 examples into 3 sets for the 3 few-shot prompts
+    bills_examples['ExampleSetNum'] = np.repeat([1, 2, 3], repeats=15/3)
+    bills_examples.to_csv(os.path.join(data_dir, 'bills_examples.csv'), index=False)
+    print(f'Saved bills_examples.csv, n = {len(bills_examples)}, at {data_dir}')
+
+    # Plot the frequency of the 10K bills over years
+    plt.figure(figsize=(10, 6))
+    bills_10k['Year'].hist(bins=range(int(bills_10k['Year'].min()), int(bills_10k['Year'].max()) + 1, 2), edgecolor='black')
+    plt.title('A histogram of the frequency of the 10K bills over years')
+    plt.xlabel('Year')
+    plt.ylabel('Number of Bills')
+    plt.grid(False)
+    plt.savefig(os.path.join(fig_dir, 'A histogram of the frequency of the 10K bills over years.png'))
+    print(f'Saved figures at {fig_dir}')
+
+# Run the main function when the script is executed
+if __name__ == "__main__":
+    main()
